58fd4bb535b04e00abcb6d0e37386cbe3ad54321
Hamad Hussain <hhtech007@proton.me>
2025-11-07 15:28:12 +0000

    first commit

---
+++ components/NavBar.js
@@ -0,0 +1,7 @@
+'use client';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import { Button } from "@/components/ui/button";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Separator } from "@/components/ui/separator";
